3b54a00e32bead575156528ccf936690c40944ff
Marcos <90912699+marcos-p4@users.noreply.github.com>
2024-03-17 10:03:31 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Me Livrando da Maldição</title>
+</head>
+<body>
+    <h1>
+        <?php
+            echo "Olá, mundo! 🌎"; 
+        ?>
+    </h1>
+    <p>Vamos tentar nos livrar da Maldição</p>
+    
+</body>
+</html>
