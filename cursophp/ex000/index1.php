475303a89a417210d7580dc35c91b39d897cc932
Marcos <90912699+marcos-p4@users.noreply.github.com>
2024-03-18 07:53:43 -0300

    Add files via upload

---
+++ cursophp/ex000/index1.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body {
+            background-color: black;
+            color: white;
+        }
+    </style>
+</head>
+<body>
+<?php
+$nome = "Marcos";
+$sobrenome = "Souza";
+const PAIS = "Brasil";
+echo "Muito prazer! Meu nome é " . $nome . " e meu sobrenome é " . $sobrenome;
+echo  "<br> Eu moro no " . PAIS;
+?>
+<script>
+    window.alert ('Você vai receber informações sobre o Marcos!')
+</script>
+</body>
+</html>
---
+++ cursophp/ex003/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos primitivos de PHP</title>
+</head>
+<body>
+    <h1>Testes de tipos primitivos</h1>
+    <?php
+        //$num = 0x1A;
+        
+        //echo "O valor da variável é $num";
+
+        //$v = 300;
+        //var_dump($v);
+
+        $vet = [1, 2, 3, 4];
+        var_dump($vet);
+        
+        
+    ?>
+    
+</body>
+</html>
